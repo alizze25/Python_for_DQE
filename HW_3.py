d26cadd83654f6afe95979f3f2e7574c1b9bd8e4
Alisa_Yavorska <Alisa_Yavorska@epam.com>
2022-10-03 02:09:13 +0300

    HW_3

--- HW_3.py
+++ HW_3.py
@@ -1,9 +1,9 @@
 import string
 
-s = '''homEwork:
+s = '''homEwork:;
   tHis iz your homeWork, copy these Text to variable.;
-  You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.;
-  it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.;
+  You NEED TO normalize it fROM letter CASEs point oF View.; also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.;
+  it iZ misspeLLing here.; fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.;
   last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87.;'''
 
 u = s.lower()  # lower all text
