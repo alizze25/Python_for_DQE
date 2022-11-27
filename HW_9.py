72f2271e3d4a07f7860192220c82e8e2a3f13f18
Alisa_Yavorska <Alisa_Yavorska@epam.com>
2022-11-27 23:23:14 +0200

    HW-9

--- HW_9.py
+++ HW_9.py
@@ -5,7 +5,7 @@
 # 3.Remove file if it was successfully processed
 
 
-#hw_7
+# hw_8
 from datetime import datetime
 import random
 import json
@@ -15,6 +15,8 @@ from operator import itemgetter
 import re
 import csv
 from HW_4_final import *
+import xml.etree.ElementTree as ET
+
 mainFilePAth = "C:\/Users\Alisa_Yavorska\PycharmProjects\Python_for_DQE\/news_feed.txt"
 defaultFilePath = "C:\/Users\Alisa_Yavorska\PycharmProjects\Python_for_DQE\/weather.txt"
 fn = ("C:\/Users\Alisa_Yavorska\PycharmProjects\Python_for_DQE\/news_feed.txt")
@@ -25,7 +27,9 @@ menu_options = {
     3: 'Scary story',
     4: 'Weather forecast',
     5: 'Json Data',
-    6: 'Exit',
+    6: 'Xml Data',
+    7: 'Api data',
+    8: 'Exit',
 }
 curDT = datetime.now()
 curDT1 = datetime.now()
@@ -33,17 +37,22 @@ random_number = random.randint(1, 10)
 file_name = 'news_feed.txt'
 # current date
 date_time = curDT.strftime("%m/%d/%Y")
+
+
 def print_menu():
     for key in menu_options.keys():
-        print (key, '--', menu_options[key] )
+        print(key, '--', menu_options[key])
 
-def option0():
+
+def create_file():
     global file_name
     file_name = input('Filename with extension, e.g. example.txt: ')
-     #print('Handle option \'Add news\'')
-def option1():
-    #file_name = input('Filename with extension, e.g. example.txt: ')
-    #with open(file_name, 'w', encoding='utf-8') as f:
+    # print('Handle option \'Add news\'')
+
+
+def write_news():
+    # file_name = input('Filename with extension, e.g. example.txt: ')
+    # with open(file_name, 'w', encoding='utf-8') as f:
     f = open(file_name, 'a', encoding='utf-8')
     f.write(input('News: '))
     f.write("\n")
@@ -55,7 +64,7 @@ def option1():
     f.write("\n")
     f.write("\n")
     f.close()
-    #Count words
+    # Count words
     try:
         os.remove("wordCount.csv")
         os.remove("count_all_file.csv")
@@ -92,7 +101,7 @@ def option1():
             # for key in list(d.keys()):
             # print(key, ":", d[key])
 
-                # deleting the csv file
+            # deleting the csv file
 
             f = open("wordCount.csv", "a", encoding='UTF8')
             for key in list(d.keys()):
@@ -100,7 +109,7 @@ def option1():
                 f.write(":")
                 f.write(str(d[key]))
                 f.write("\n")
-     #Create second csv file
+    # Create second csv file
     file = open("C:\/Users\Alisa_Yavorska\PycharmProjects\Python_for_DQE\/news_feed.txt", "r")
     data = file.read()
     # get the length of the data
@@ -150,8 +159,9 @@ def option1():
             writer.writerow(["Letters count", "All words count", "Uppercase count", "Percentage count"])
             writer.writerow([number_of_characters, len(words), count, str(res)])
 
-def option2():
-    #file_name = input('Filename with extension, e.g. example.txt: ')
+
+def write_ads():
+    # file_name = input('Filename with extension, e.g. example.txt: ')
     f = open(file_name, 'a', encoding='utf-8')
     f.write("\n")
     f.write("\n")
@@ -261,8 +271,9 @@ def option2():
                     writer.writerow(["Letters count", "All words count", "Uppercase count", "Percentage count"])
                     writer.writerow([number_of_characters, len(words), count, str(res)])
 
-def option3():
-    #file_name = input('Filename with extension, e.g. example.txt: ')
+
+def write_scarystory():
+    # file_name = input('Filename with extension, e.g. example.txt: ')
     f = open(file_name, 'a', encoding='utf-8')
     f.write("\n")
     f.write("\n")
@@ -331,12 +342,12 @@ def option3():
     # count_uppercase
 
     with open("news_feed.txt") as file:
-     count = 0
-     text = file.read()
+        count = 0
+        text = file.read()
     for i in text:
-     if i.isupper():
-         count += 1
-         print(count)
+        if i.isupper():
+            count += 1
+            print(count)
 
     file = open("news_feed.txt", "rt")
     data = file.read()
@@ -368,7 +379,8 @@ def option3():
             writer.writerow(["Letters count", "All words count", "Uppercase count", "Percentage count"])
             writer.writerow([number_of_characters, len(words), count, str(res)])
 
-def option4():
+
+def write_weather():
     file_path = input('Enter a file path: ')
     if os.path.exists(file_path):
         with open(file_path, 'r', encoding='utf-8-sig') as f:
@@ -387,7 +399,7 @@ def option4():
             fullFIle.write('Weather forecast: ')
             fullFIle.write("\n")
             fullFIle.write(Beautifyer(lines1))
-            #os.remove(defaultFilePath)
+            # os.remove(defaultFilePath)
             # Count words
             try:
                 os.remove("wordCount.csv")
@@ -481,7 +493,9 @@ def option4():
             writer = csv.writer(outcsv)
             writer.writerow(["Letters count", "All words count", "Uppercase count", "Percentage count"])
             writer.writerow([number_of_characters, len(words), count, str(res)])
-def option5():
+
+
+def write_jsondata():
     json_path = input('Enter valid json path: ')
     if os.path.exists(json_path):
         with open(json_path, 'r') as openfile:
@@ -492,10 +506,12 @@ def option5():
             # resp = json.dumps(openfile.__dict__)
             # o = json.loads(resp)
             data = json.load(openfile)
+            json_fields_count = input('Choose how many records should be added(write one or two): ')
+    if json_fields_count == 'one':
         if 'row1' in data:
             for i in data['row1']:
                 if i['type1'] == "news":
-                    f.write("Name: " )
+                    f.write("Name: ")
                     f.write(i['type1'])
                     f.write("\n")
                     f.write("Text: ")
@@ -504,9 +520,10 @@ def option5():
                     f.write("From: ")
                     f.write(i['city'])
                     f.write("\n")
+                    f.write("\n")
 
                 elif i['type1'] == "ads":
-                    f.write("Type: ")
+                    f.write("Name: ")
                     f.write(i['type1'])
                     f.write("\n")
                     f.write("ads_text: ")
@@ -515,8 +532,37 @@ def option5():
                     f.write("ads_date: ")
                     f.write(i['ads_date'])
                     f.write("\n")
+                    f.write("\n")
                     print()
+        os.remove(json_path)
 
+    elif json_fields_count == 'two':
+        if 'row1' in data:
+            for i in data['row1']:
+                if i['type1'] == "news":
+                    f.write("Name: ")
+                    f.write(i['type1'])
+                    f.write("\n")
+                    f.write("Text: ")
+                    f.write(i['text_news'])
+                    f.write("\n")
+                    f.write("From: ")
+                    f.write(i['city'])
+                    f.write("\n")
+                    f.write("\n")
+
+                elif i['type1'] == "ads":
+                    f.write("Name: ")
+                    f.write(i['type1'])
+                    f.write("\n")
+                    f.write("ads_text: ")
+                    f.write(i['ads_text'])
+                    f.write("\n")
+                    f.write("ads_date: ")
+                    f.write(i['ads_date'])
+                    f.write("\n")
+                    f.write("\n")
+                    print()
         if 'row2' in data:
             for i in data['row2']:
                 if i['type2'] == "news":
@@ -530,9 +576,10 @@ def option5():
                     f.write("From: ")
                     f.write(i['city'])
                     f.write("\n")
+                    f.write("\n")
                 elif i['type2'] == "ads":
                     f.write("\n")
-                    f.write("Type: ")
+                    f.write("Name: ")
                     f.write(i['type2'])
                     f.write("\n")
                     f.write("ads_text: ")
@@ -541,8 +588,11 @@ def option5():
                     f.write("ads_date: ")
                     f.write(i['ads_date'])
                     f.write("\n")
+                    f.write("\n")
+                    os.remove(json_path)
         else:
             print("Invalid json")
+
     else:
         with open('try.json', 'r') as openfile:
             data = json.load(openfile)
@@ -560,9 +610,10 @@ def option5():
                     f.write("From: ")
                     f.write(i['city'])
                     f.write("\n")
+                    f.write("\n")
 
                 elif i['type1'] == "ads":
-                    f.write("Type: ")
+                    f.write("Name: ")
                     f.write(i['type1'])
                     f.write("\n")
                     f.write("ads_text: ")
@@ -570,6 +621,7 @@ def option5():
                     f.write("\n")
                     f.write("ads_date: ")
                     f.write(i['ads_date'])
+                    f.write("\n")
                     f.write("\n")
                     print()
 
@@ -586,6 +638,7 @@ def option5():
                     f.write("From: ")
                     f.write(i['city'])
                     f.write("\n")
+                    f.write("\n")
                 elif i['type2'] == "ads":
                     f.write("\n")
                     f.write("Type: ")
@@ -597,30 +650,180 @@ def option5():
                     f.write("ads_date: ")
                     f.write(i['ads_date'])
                     f.write("\n")
+                    f.write("\n")
+
         else:
             print("Invalid json")
-if __name__=='__main__':
-    while(True):
+
+# Count words
+    try:
+        os.remove("wordCount.csv")
+        os.remove("count_all_file.csv")
+    except OSError:
+        pass
+    text = open("news_feed.txt", "r")
+
+    # Create an empty dictionary
+    d = dict()
+
+    # Loop through each line of the file
+    for line in text:
+        # Remove the leading spaces and newline character
+        line = line.strip()
+
+        # Convert the characters in line to
+        # lowercase to avoid case mismatch
+        line = line.lower()
+
+        # Split the line into words
+        words = line.split(" ")
+
+        # Iterate over each word in line
+        for word in words:
+            # Check if the word is already in dictionary
+            if word in d:
+                # Increment count of word by 1
+                d[word] = d[word] + 1
+            else:
+                # Add the word to dictionary with count 1
+                d[word] = 1
+
+            # Print the contents of dictionary
+            # for key in list(d.keys()):
+            # print(key, ":", d[key])
+
+            # deleting the csv file
+
+            f = open("wordCount.csv", "a", encoding='UTF8')
+            for key in list(d.keys()):
+                f.write(str(key))
+                f.write(":")
+                f.write(str(d[key]))
+                f.write("\n")
+            # Create second csv file
+            file = open("C:\/Users\Alisa_Yavorska\PycharmProjects\Python_for_DQE\/news_feed.txt", "r")
+            data = file.read()
+            # get the length of the data
+            number_of_characters = len(data)
+
+            print('Number of characters in text file :', number_of_characters)
+
+            # count_uppercase
+
+            with open("news_feed.txt") as file:
+                count = 0
+                text = file.read()
+                for i in text:
+                    if i.isupper():
+                        count += 1
+                print(count)
+
+                file = open("news_feed.txt", "rt")
+                data = file.read()
+                words = data.split()
+
+                print('Number of words in text file :', len(words))
+
+                # count percentage
+                # text = "Alice opened the door and found that it led into a small passage, not much larger than a rat-hole: she knelt down and looked along the passage into the loveliest garden you ever saw."
+                with open(fn) as f:
+                    data2 = f.read()
+                    # concatenating using join
+                    joined = " ".join(ele for ele in data2)
+
+                    # mapping using Counter()
+                    mappd = Counter(joined.split())
+
+                    # getting total using sum
+                    total_val = sum(mappd.values())
+
+                    # getting share of each word
+                    res = {key: val / total_val for key,
+                                                    val in mappd.items()}
+
+                    # printing result
+                    print("Percentage share of each word : " + str(res))
+
+                # Write to csv
+                with open('count_all_file.csv', 'w', newline='') as outcsv:
+                    writer = csv.writer(outcsv)
+                    writer.writerow(["Letters count", "All words count", "Uppercase count", "Percentage count"])
+                    writer.writerow([number_of_characters, len(words), count, str(res)])
+def write_xmldata():
+    xml_path = input('Enter valid xml path: ')
+    if os.path.exists(xml_path):
+        f = open(file_name, 'a', encoding='utf-8')
+        f.write("\n")
+        mytree = ET.parse(xml_path)
+        myroot = mytree.getroot()
+        for x in myroot.findall('employee'):
+                item = x.find('item').text
+                home = x.find('home').text
+                job_description = x.find('job_description').text
+                work_experience = x.find('work_experience').text
+                f.write("\n")
+                #print(item, home, job_description, work_experience)
+                f.write("Job search")
+                f.write("\n")
+                f.write("Employee name: " + item)
+                f.write("\n")
+                f.write("Employee location: " + home)
+                f.write("\n")
+                f.write("Employee position: " + job_description)
+                f.write("\n")
+                f.write("Employee work experience: " + work_experience)
+                f.write("\n")
+    else:
+        mytree = ET.parse('tryXml2.xml')
+        myroot = mytree.getroot()
+        f = open(file_name, 'a', encoding='utf-8')
+        f.write("\n")
+        for x in myroot.findall('employee'):
+            item = x.find('item').text
+            home = x.find('home').text
+            job_description = x.find('job_description').text
+            work_experience = x.find('work_experience').text
+            # print(item, home, job_description, work_experience)
+            f.write("\n")
+            # print(item, home, job_description, work_experience)
+            f.write("Job search")
+            f.write("\n")
+            f.write("Employee name: " + item)
+            f.write("\n")
+            f.write("Employee location: " + home)
+            f.write("\n")
+            f.write("Employee position: " + job_description)
+            f.write("\n")
+            f.write("Employee work experience: " + work_experience)
+            f.write("\n")
+def write_apidata():
+    print("Hello")
+if __name__ == '__main__':
+    while (True):
         print_menu()
         option = ''
         try:
             option = int(input('Enter your choice: '))
         except:
             print('Wrong input. Please enter a number ...')
-        #Check what choice was entered and act accordingly
+        # Check what choice was entered and act accordingly
         if option == 0:
-           option0()
+            create_file()
         if option == 1:
-           option1()
+            write_news()
         elif option == 2:
-            option2()
+            write_ads()
         elif option == 3:
-            option3()
+            write_scarystory()
         elif option == 4:
-            option4()
+            write_weather()
         elif option == 5:
-            option5()
+            write_jsondata()
         elif option == 6:
+            write_xmldata()
+        elif option == 7:
+            write_apidata()
+        elif option == 8:
             print('Thanks message before exiting')
             exit()
         else:
