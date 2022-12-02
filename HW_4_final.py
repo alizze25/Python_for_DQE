ed04ef2d52a82e0aa7d3befa1e702cba327888ca
Alisa_Yavorska <Alisa_Yavorska@epam.com>
2022-12-02 10:51:46 +0200

    HW-4

--- HW_4_final.py
+++ HW_4_final.py
@@ -1,6 +1,7 @@
 #  add func for hw3
 
 import string
+import re
 
 sText = '''homEwork:
   tHis iz your homeWork, copy these Text to variable.
@@ -14,20 +15,32 @@ def Beautifyer(s):
     print(word)
     x = word
     try1 = '\n'.join(
-        [i.strip().capitalize() + '.' for i in x.split('.')])  # Capitalize all first words after period and start from new line\
+        [i.strip().capitalize() for i in
+         x.split(';')])  # Capitalize all first words after period and start from new line\
     print(try1)
     x12 = try1.replace("iz", "is")
     print(x12)
     x13 = x12.replace("normalise", "normalize")
-    x13.strip().rstrip('.')
     print(x13)
+
+    # check count whitespace
+
+    # Program for counting number of spaces in text
+
     c = 0
     for i in s:
         if (i.isspace()):
             c += 1
-
     print("Number of Spaces : " + str(c))
-    return x13
+
+    # Create one sentences with last words
+
+    z = re.findall(r'(\w+)\.', try1)
+    print(z)
+    for i in z:
+        try1 = try1 + ' ' + i
+    print(try1)
+
 
 Beautifyer(sText)
 # check count whitespace
